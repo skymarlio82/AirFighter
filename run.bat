271aa169509b92a3a422289f73ec1e289d6f0ec1
marlio <jitao.liu82@gmail.com>
2020-12-27 23:38:00 +0800

    change for run script

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 
 @echo off
 
-set MIDP_HOME=C:\Users\MLIU22\_Jitao\software\libs\midp2.0fcs
+set MIDP_HOME=C:\Users\jitao\_marlio\software\J2meDevKit\midp2.0fcs
 set CLASSPATH=.;.\classes;
 
 %MIDP_HOME%\bin\midp -classpath %CLASSPATH% org.migame.airfighter.midlet.AirFighterMIDlet
